1078a69b2fa8ca022264f7e7a6e72e7bc50765d3
Rajashekar Reddy Busireddy <busireddyrajashekar@gmail.com>
2019-07-17 07:22:20 +0530

    Updates

---
+++ precache-manifest.ffc9e3b8d086751d2dde1ce894aada87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472ae3ca442d8184f7e7bfb80ab9cb76",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3d1cc7314268c2c02645",
+    "url": "/monsters-rolodex/static/css/main.ee578a5a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3d1cc7314268c2c02645",
+    "url": "/monsters-rolodex/static/js/main.209a81b6.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
